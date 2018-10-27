e428d26b34ff749de6081c6765781ba5d12be77f
Vitoria Tai <vitoriatai@Vitorias-MacBook-Air.local>
2018-10-27 17:44:12 -0400

    Initial commit

---
+++ app/src/main/java/com/example/vitoriatai/flashcard/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.vitoriatai.flashcard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
+                findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
+
+            }
+        });
+
+        findViewById(R.id.flashcard_answer).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
+                findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
+            }
+        });
+
+        findViewById(R.id.choice_one).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.choice_one).setBackgroundColor(
+                        getResources().getColor(R.color.red, null));
+                findViewById(R.id.choice_two).setBackgroundColor(
+                        getResources().getColor(R.color.green, null));
+            }
+        });
+
+        findViewById(R.id.choice_two).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.choice_two).setBackgroundColor(
+                        getResources().getColor(R.color.green, null));
+            }
+        });
+
+        findViewById(R.id.choice_three).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.choice_three).setBackgroundColor(
+                        getResources().getColor(R.color.red, null));
+                findViewById(R.id.choice_two).setBackgroundColor(
+                        getResources().getColor(R.color.green, null));
+            }
+        });
+
+        findViewById(R.id.rootview).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.choice_one).setBackgroundColor(
+                        getResources().getColor(R.color.orange));
+                findViewById(R.id.choice_two).setBackgroundColor(
+                        getResources().getColor(R.color.orange));
+                findViewById(R.id.choice_three).setBackgroundColor(
+                        getResources().getColor(R.color.orange));
+            }
+        });
+
+        findViewById(R.id.toggle_choices_visibility).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((ImageView) findViewById(R.id.toggle_choices_visibility)).setImageResource(R.drawable.eyeclosed);
+
+            }
+        });
+
+
+    }
+}
